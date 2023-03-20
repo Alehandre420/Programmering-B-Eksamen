573294878e1e28b62939522007564c5b77169793
Alehandre420 <alexandernikolajlarsen@gmail.com>
2023-03-20 11:21:49 +0100

    Tower make start

--- Programmering B Eksamen Projekt/Assets/Scripts/TowerSpawn.cs
+++ Programmering B Eksamen Projekt/Assets/Scripts/TowerSpawn.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pathfinding : MonoBehaviour
+public class TowerSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print("hello farmor");
+        
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
