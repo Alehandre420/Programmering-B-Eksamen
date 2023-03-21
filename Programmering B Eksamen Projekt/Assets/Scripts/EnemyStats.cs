fb3e3fe2a858bea9a9140c69c45a98de01060cf9
Mikkelgan <mikk478n@elev.aatg.dk>
2023-03-21 09:40:18 +0100

    Basic enemie

---
+++ Programmering B Eksamen Projekt/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float health;
+    public float speed;
+    public float worth;
+    public float damage;
+
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            //gm.money += worth;
+            Destroy(gameObject);
+        }
+    }
+}
