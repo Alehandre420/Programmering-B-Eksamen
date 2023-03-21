52868be5ca679df12b616251caf0d3574ba63457
Alehandre420 <alexandernikolajlarsen@gmail.com>
2023-03-21 10:45:08 +0100

    Tower attack state
    started making the attack state

--- Programmering B Eksamen Projekt/Assets/Scripts/TowerScript.cs
+++ Programmering B Eksamen Projekt/Assets/Scripts/TowerScript.cs
@@ -7,8 +7,11 @@ public class TowerScript : MonoBehaviour
     public float damage;
     public float atkSpeed;
     public float cost;
+    public List<GameObject> enemyList;
+    public EnemyStats enemyStats;
 
     bool enemyInRadius;
+    bool isInterrupted;
 
     private void Awake()
     {
@@ -17,6 +20,10 @@ public class TowerScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Enemy") && !enemyList.Contains(other.gameObject))
+        {
+            enemyList.Add(other.gameObject);
+        }
         if (other.CompareTag("Enemy"))
         {
             enemyInRadius = true;
@@ -25,15 +32,34 @@ public class TowerScript : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.CompareTag("Enemy") && enemyList.Contains(other.gameObject))
+        {
+            enemyList.Remove(other.gameObject);
+        }
         if (other.CompareTag("Enemy"))
         {
             enemyInRadius = false;
         }
     }
 
+    IEnumerator attackEnemy()
+    {
+        isInterrupted = true;
+        print("gtims");
+        //remove 10 health from enemy in list
+        yield return new WaitForSeconds(2);
+        isInterrupted = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            if (!isInterrupted)
+            {
+                StartCoroutine(attackEnemy());
+            }
+        }
     }
 }
